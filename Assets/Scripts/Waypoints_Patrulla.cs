220d2d3a58e4ab98b163ab33e9b7bc6aa2b9fff8
jrguez03 <jokorguez@gmail.com>
2024-02-25 22:00:02 +0000

    rotascion de el enemigo

--- Assets/Scripts/Waypoints_Patrulla.cs
+++ Assets/Scripts/Waypoints_Patrulla.cs
@@ -9,6 +9,7 @@ public class Waypoints_Patrulla : MonoBehaviour
     int w_CurrentWaypointIndex;
 
     public float w_MoveSpeed = 20f;
+    public float w_RotationSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +31,10 @@ public class Waypoints_Patrulla : MonoBehaviour
         Transform currentWaypoint = waypoints[w_CurrentWaypointIndex];
 
         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, w_MoveSpeed * Time.deltaTime);
+
+        Vector3 waypointForward = (currentWaypoint.position - transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(waypointForward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, w_RotationSpeed * Time.deltaTime);
 
         if (Vector3.Distance(transform.position, currentWaypoint.position) < 0.1f)
         {
